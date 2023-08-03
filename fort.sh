76dd1ab3a9b8eb78032d5e627a7fd8935f0f2e89
SetScript <101998415+SetScript@users.noreply.github.com>
2023-08-03 11:01:13 +0300

    Update fort.sh

--- fort.sh
+++ fort.sh
@@ -2,6 +2,6 @@
 
 ruby /bin/fort.rb $*
 
-# move this file to /bin/
-# rename this file to "fort"
+# move this file to /bin/ directory. 
+# rename this file to "fort".
 # write "chmod +x fort" from /bin/ directory. 
